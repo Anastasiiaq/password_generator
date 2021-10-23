124d821e1b01affed2c41492b4bdc3e282d713b4
anastasiia <a.ananas11@yandex.ru>
2021-10-23 18:49:24 +0300

    some refactoring, added lowercase generate

--- main.c
+++ main.c
@@ -28,19 +28,19 @@ void	new_params(t_param **passwd_params)
 		error_allocation();
 }
 
-void	init_passwd_params(t_param **passwd_params)
+void	init_passwd_params(t_param *passwd_params)
 {
-	(*passwd_params)->count_symb = 0;
-	(*passwd_params)->lowercase_letter = 0;
-	(*passwd_params)->uppercase_letter = 0;
-	(*passwd_params)->num = 0;
-	(*passwd_params)->special_symb = 0;
-	(*passwd_params)->count_params = 0;
+	passwd_params->count_symb = 0;
+	passwd_params->lowercase_letter = -1;
+	passwd_params->uppercase_letter = -1;
+	passwd_params->num = -1;
+	passwd_params->special_symb = -1;
+	passwd_params->count_params = 0;
 }
 
-void	get_count_param(int argc, t_param **passwd_params)
+void	get_count_param(int argc, t_param *passwd_params)
 {
-	(*passwd_params)->count_params = argc - 1;
+	passwd_params->count_params = argc - 1;
 }
 
 int	arg_is_num(char *argv)
@@ -60,26 +60,41 @@ void	arg_is_num_err()
 	exit(-1);
 }
 
-void	set_param(char **argv, t_param **passwd_params)
+void	set_param(char **argv, t_param *passwd_params)
 {
 	if (arg_is_num(argv[1]) != 0)
 		arg_is_num_err();
-	(*passwd_params)->count_symb = atoi(argv[1]);
-	if ((*passwd_params)->count_symb == 0)
+	passwd_params->count_symb = atoi(argv[1]);
+	if (passwd_params->count_symb == 0)
 		return ;
 	argv += 2;
 	while (*argv != NULL)
 	{
 		if (**argv == 'a')
-			(*passwd_params)->lowercase_letter = 1;
+			passwd_params->lowercase_letter = 1;
 		else if (**argv == 'A')
-			(*passwd_params)->uppercase_letter = 1;
+			passwd_params->uppercase_letter = 1;
 		else if (**argv == '1')
-			(*passwd_params)->num = 1;
+			passwd_params->num = 1;
 		else if (**argv == '!')
-			(*passwd_params)->special_symb = 1;
+			passwd_params->special_symb = 1;
 		argv++;
 	}
+}
+
+void	set_order_num(t_param *passwd_params)
+{
+	int	num;
+	
+	num = 0;
+	if (passwd_params->lowercase_letter == 1)
+		passwd_params->lowercase_letter = num++;
+	if (passwd_params->uppercase_letter == 1)
+		passwd_params->uppercase_letter = num++;
+	if (passwd_params->num == 1)
+		passwd_params->num = num++;
+	if (passwd_params->special_symb == 1)
+		passwd_params->special_symb = num++;
 }
 
 void	new_passwd(char **passwd, t_param *passwd_params)
@@ -89,12 +104,13 @@ void	new_passwd(char **passwd, t_param *passwd_params)
 		error_allocation();
 }
 
-void	random_symb(t_param *passwd_params)
+long int	random_order_num(t_param *passwd_params)
 {
 	long int num;
 	
-	srandom((unsigned int)time(NULL));
-	num = random() % passwd_params->count_params;
+	srandom(passwd_params->count_params * getpid() + (unsigned int)random());
+	num = random() % (passwd_params->count_params - 1);
+	return (num);
 }
 
 char	*get_num_symb(long int index)
@@ -109,28 +125,94 @@ char	*get_num_symb(long int index)
 	return (symb);
 }
 
+void	set_num_symb(char *passwd, int iteration)
+{
+	long int	num;
+	char		*symb;
+	
+	srandom((iteration + 1) * getpid() + (unsigned int)random());
+	num = random() % 10;
+	symb = get_num_symb(num);
+	if (strlen(passwd) > 0 && passwd[iteration - 1] == symb[0])
+	{
+//		free(symb);
+		set_num_symb(passwd, iteration);
+//		return ;
+	}
+	strcat(passwd, symb);
+	free(symb);
+}
+
+char	*get_lowercase_symb(long int index)
+{
+	char	*base;
+	char	*symb;
+	
+	base = "abcdefghijklmnopqrstuvwxyz";
+	symb = (char *)malloc(sizeof(char) * 2);			// проверка маллока
+	symb[0] = base[index];
+	symb[1] = '\0';
+	return (symb);
+}
+
+void	set_lowercase_symb(char *passwd, int iteration)
+{
+	long int	num;
+	char		*symb;
+	
+	srandom((iteration + 1) * getpid() + (unsigned int)random());
+	num = random() % 26;
+	symb = get_lowercase_symb(num);
+	if (strlen(passwd) > 0 && passwd[iteration - 1] == symb[0])
+		set_lowercase_symb(passwd, iteration);
+	strcat(passwd, symb);
+	free(symb);
+}
+
 void	print_passwd(t_param *passwd_params)
 {
 	char	*passwd;
-	char	*symb;
+//	char	*symb;
+	long int	rand_ord_num;
 	// tmp
 	int		i;
-	long int num;
+//	long int num;
+	
+	
+	set_order_num(passwd_params);
 	
 	new_passwd(&passwd, passwd_params);
-//	random_symb(passwd_params);
-	
 	i = 0;
 	while (i < passwd_params->count_symb)
 	{
-		srandom((i + 1) * getpid() + (unsigned int)random());
-		num = random() % 10;
-		symb = get_num_symb(num);
-		if (strlen(passwd) > 0 && passwd[i - 1] == symb[0])
-			continue ;
-		strcat(passwd, get_num_symb(num));
+		rand_ord_num = random_order_num(passwd_params);
+		if (passwd_params->lowercase_letter == rand_ord_num)
+			set_lowercase_symb(passwd, i);
+		else if (passwd_params->uppercase_letter == rand_ord_num)
+			;
+//			set_uppercase_symb(passwd, i);
+		else if (passwd_params->num == rand_ord_num)
+			set_num_symb(passwd, i);
+		else if (passwd_params->special_symb == rand_ord_num)
+			;
+//			set_special_symb(passwd, i);
 		i++;
 	}
+//	random_symb(passwd_params);
+	
+	
+	
+//	i = 0;
+//	while (i < passwd_params->count_symb)
+//	{
+//		srandom((i + 1) * getpid() + (unsigned int)random());
+//		num = random() % 10;
+//		symb = get_num_symb(num);
+//		if (strlen(passwd) > 0 && passwd[i - 1] == symb[0])
+//			continue ;
+//		strcat(passwd, get_num_symb(num));
+//		i++;
+//	}
 	puts("Generated password:");
 	puts(passwd);
 }
@@ -139,15 +221,18 @@ int main(int argc, char **argv)
 {
 	t_param	*passwd_params;
 	
-	if (argc > 1)
+	if (argc >= 3 && argc <= 6)
 	{
 		new_params(&passwd_params);
-		init_passwd_params(&passwd_params);
-		get_count_param(argc, &passwd_params);
-		set_param(argv, &passwd_params);
+		init_passwd_params(passwd_params);
+		get_count_param(argc, passwd_params);
+		set_param(argv, passwd_params);
 		print_passwd(passwd_params);
 	}
-	else
+	else if (argc < 3)
 		puts("Error: too few arguments");
+	else
+		puts("Error: too many arguments");
+	
 	return 0;
 }
