820c325bed76168894169b7d092071b8e8761132
anastasiia <a.ananas11@yandex.ru>
2021-10-29 00:00:59 +0300

    fixed the ability to enter a string of invalid characters, for example 'aaa', 'Aaa', '1cd', etc.

--- src/set_password_params.c
+++ src/set_password_params.c
@@ -21,13 +21,13 @@ void	set_param(char **argv, t_param *passwd_params)
 	argv += 2;
 	while (*argv != NULL)
 	{
-		if (**argv == 'a' && passwd_params->lowercase_letter == -1)
+		if (strcmp(*argv, "a") == 0 && passwd_params->lowercase_letter == -1)
 			passwd_params->lowercase_letter = 1;
-		else if (**argv == 'A' && passwd_params->uppercase_letter == -1)
+		else if (strcmp(*argv, "A") == 0 && passwd_params->uppercase_letter == -1)
 			passwd_params->uppercase_letter = 1;
-		else if (**argv == '1' && passwd_params->num == -1)
+		else if (strcmp(*argv, "1") == 0 && passwd_params->num == -1)
 			passwd_params->num = 1;
-		else if (**argv == '!' && passwd_params->special_symb == -1)
+		else if (strcmp(*argv, "!") == 0 && passwd_params->special_symb == -1)
 			passwd_params->special_symb = 1;
 		else
 			param_err();
